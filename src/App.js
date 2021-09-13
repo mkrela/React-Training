9b58be1eea324379712184638b95467345672259
mkrela <77677471+mkrela@users.noreply.github.com>
2021-09-13 15:17:14 +0200

    Test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 import React, {Component} from "react";
+import { Fragment } from 'react/cjs/react.production.min';
 
 class App extends Component{
   render () {
     return (
+      <Fragment>
       <div className='App'>
         <h1>TUTO REACT</h1>
+        <p></p>
       </div>
+      </Fragment>
     )
   }
 }
