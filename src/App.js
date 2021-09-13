fd00fe748bdd23ce624aaf200ee3cd8f641dc0dc
mkrela <77677471+mkrela@users.noreply.github.com>
2021-09-13 14:51:18 +0200

    PremierCommit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+import React, {Component} from "react";
+
+class App extends Component{
+  render () {
+    return (
+      <div className='App'>
+        <h1>TUTO REACT</h1>
+      </div>
+    )
+  }
+}
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <h1>React APP on va apprendre</h1>
+//       </header>
+//     </div>
+//   );
+// }
+
+export default App;
