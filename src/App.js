c6959c4809cf16ad40897fba547d5d5186211c49
mkrela <77677471+mkrela@users.noreply.github.com>
2021-09-13 17:04:30 +0200

    Component Button

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import React, { Component } from "react";
 import { Fragment } from "react/cjs/react.production.min";
 import Membre from "./components/Membre";
+import Button from "./components/button";
 
 const famille = {
   membre1: { nom: "Matthieu", age: 27 },
@@ -13,11 +14,11 @@ class App extends Component {
     famille,
   };
 
-  handleClick = () => {
+  handleClick = (num) => {
     const famille={ ...this.state.famille}
-    famille.membre1.age += 1
-    famille.membre2.age += 1 
-    famille.membre3.age += 1
+    famille.membre1.age += num
+    famille.membre2.age += num
+    famille.membre3.age += num
     this.setState({famille})
   }
 
@@ -30,8 +31,11 @@ class App extends Component {
           <h1>TUTO REACT</h1>
           <Membre nom={famille.membre1.nom} age={famille.membre1.age} />
           <Membre nom={famille.membre2.nom} age={famille.membre2.age}/>
-          <Membre nom={famille.membre3.nom} age={famille.membre3.age}/>
-          <button onClick={this.handleClick}>Vieillir</button>
+          <Membre nom={famille.membre3.nom} age={famille.membre3.age}>
+            Je suis un bon bibi qui aime son papa et sa maman </Membre>
+            <Button
+            vieillir={() => this.handleClick(1)}
+            />
         </div>
       </Fragment>
     );
---
+++ src/components/button.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Button = ({vieillir}) => {
+  return <button
+  onClick={vieillir}
+  >Vieillir</button>;
+};
+
+export default Button;
