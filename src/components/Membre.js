68a6649f9d200b47adb3eecdfe1265917d34e9ae
mkrela <77677471+mkrela@users.noreply.github.com>
2021-09-14 11:00:47 +0200

    Test de rendu dynamiques

--- src/components/Membre.js
+++ src/components/Membre.js
@@ -1,10 +1,14 @@
 import React, {Fragment} from "react";
 import { Children } from "react";
+import './Style.css';
 
 const Membre = ({nom, age, children}) => {
     return(
         <Fragment>
-       <h2>{nom} : {age}</h2>
+       <h2 style={{
+           backgroundColor: age < 10 ? 'red' : 'green',
+           color: age < 10 ? 'white' : 'black'
+           }}>{nom} : {age}</h2>
        <p>{children}</p>
        </Fragment>
     )
